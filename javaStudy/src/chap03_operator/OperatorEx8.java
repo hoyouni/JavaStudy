046d72bf75d30b60e88eb78f0b24f3e0cf3b623b
hoyouni <sgm00006@gmail.com>
2021-03-04 20:17:49 +0900

    chap03_논리연산자

---
+++ javaStudy/src/chap03_operator/OperatorEx8.java
@@ -0,0 +1,43 @@
+package chap03_operator;
+
+public class OperatorEx8 {
+	public static void main(String[] args) {
+		String str1 = "abc";
+		String str2 = new String("abc");
+		
+		System.out.println("str1 == str2 : " + (str1 == str2)); // 두 변수는 서로 다른 객체이므로 == 연산자는 false를 return
+		System.out.println("str1.equals(str2) : " + (str1.equals(str2))); // equals() 함수는 다른 객체더라도 내용이 같으면 true 출력 
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
