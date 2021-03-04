c22b481aa480c3c236739fdc9e8d6b6b621cbec2
hoyouni <sgm00006@gmail.com>
2021-03-04 19:25:02 +0900

    chap02_Casting

---
+++ javaStudy/src/chap02/CastingEx1.java
@@ -0,0 +1,45 @@
+package chap02;
+
+// 형변환 Casting
+public class CastingEx1 {
+	public static void main(String[] args) {
+		double d = 85.4;
+		int score = (int)d;
+		System.out.println(score);
+		
+		float f = 1.6f;
+		int score2 = (int)f;
+		System.out.println(score2); // 버림 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javaStudy/src/chap02/CharToCode.java
@@ -0,0 +1,51 @@
+package chap02;
+
+public class CharToCode {
+	public static void main(String[] args) {
+		char ch  = 'A';
+		int code = ch; // 형변환 해줘도 되지만 자동으로 형변환 되므로 생략 가능 
+		
+		System.out.println(code);
+		
+		char ch2 = '가';
+		int code2 = ch2;
+		
+		System.out.println(code2);
+		
+		// 실제로 char 타입의 변수에는 문자가 아닌 문자의 유니코드(정수)가 저장되고 표현형식 역시 정수형과 동일 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
