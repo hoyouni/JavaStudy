83c77d9ffc320f8f7f9ed9d78f8434de07989384
hoyouni <sgm00006@gmail.com>
2021-03-04 15:28:07 +0900

    test new Repository

---
+++ javaStudy/src/testGit/TestGit.java
@@ -0,0 +1,38 @@
+package testGit;
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("first test");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
