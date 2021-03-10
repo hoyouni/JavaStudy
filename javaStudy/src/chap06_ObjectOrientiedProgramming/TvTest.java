6e7424b6bca27a0f8af5d31c6c58f973fdf3b0f0
hoyouni <sgm00006@gmail.com>
2021-03-10 17:24:56 +0900

    test commit

--- javaStudy/src/chap06_ObjectOrientiedProgramming/TvTest.java
+++ javaStudy/src/chap06_ObjectOrientiedProgramming/TvTest.java
@@ -24,9 +24,9 @@ public class TvTest {
 	public static void main(String[] args) {
 		Tv t; // Tv 인스턴스를 참조하기 위한 변수 t 선언
 		t = new Tv(); // Tv 인스턴스 생성 후 변수 참조
-		t.channel = 7; 
+		t.channel = 7;
 		t.channelDown();
-		System.out.println("현재 채널은 " + t.channel + " 입니다. ");
+		System.out.println("현재 채널은 " + t.channel + " 입니다.");
 	}
 }
 
