01c0af7b6b7ff412704199ac122c79504a918e7a
hoyouni <sgm00006@gmail.com>
2021-03-10 18:37:50 +0900

    call by Value, call by Reference

---
+++ javaStudy/src/chap06_ObjectOrientiedProgramming/ReferenceParamEx.java
@@ -0,0 +1,27 @@
+package chap06_ObjectOrientiedProgramming;
+
+// call by Reference
+
+/*
+class Data {
+	int x;
+}
+*/
+
+public class ReferenceParamEx {
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+
+		change(d);
+		
+		System.out.println("change after main() : x = " + d.x);
+		
+	}
+	
+	static void change(Data d) {
+		d.x = 1000;
+		System.out.println("change( : x = " + d.x);
+	}
+}
