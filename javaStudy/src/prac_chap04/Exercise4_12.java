6b7104da5cdadb08aa59c3940212618b1f733ccb
hoyouni <sgm00006@gmail.com>
2021-04-02 16:21:49 +0900

    chap04_반복문과 조건문_연습문제 풀이

---
+++ javaStudy/src/prac_chap04/Exercise4_12.java
@@ -0,0 +1,17 @@
+package prac_chap04;
+
+public class Exercise4_12 {
+	public static void main(String[] args) {
+		// 구구단
+		// 다시 풀어야함
+		
+		/*
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 2; j <= i; j++) {
+				System.out.print(j + "*" + i + "=" + (i * j));
+			}
+			System.out.println();
+		}
+		*/
+	}
+}
